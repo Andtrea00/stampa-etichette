acd51271b9a969a5a8aa6b6d326cd8ec2a4dd727
fontanelliandrea31@gmail.com <tuaemail@example.com>
2025-09-29 09:01:27 +0200

    Use persistent disk for SQLite db

--- print_label/settings.py
+++ print_label/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+DB_PATH = os.environ.get("DB_PATH", str(BASE_DIR / "db.sqlite3"))
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -77,7 +79,7 @@ WSGI_APPLICATION = 'print_label.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': DB_PATH,
     }
 }
 
